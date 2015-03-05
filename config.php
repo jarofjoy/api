d2bad4db1c3a10110530b0eb8961162e5831707a
Kanishk Vashisht <kanishk.vashisht@gmail.com>
2015-03-05 14:35:02 +0530

    added db configuration

--- config.php
+++ config.php
@@ -1,21 +1,10 @@
-<?php
-<?php
-
- 			/*
- 			$mysql_host = "mysql15.000webhost.com";
-$mysql_database = "a8591040_jarmain";
-$mysql_user = "a8591040_admin";
-$mysql_password = "jarofjoy123";
- 			*/
-
-	$db_host = "";
-	$db_user = "";
-	$db_database = "";
-	$db_pass = "";
-	$con=mysqli_connect($db_host, $db_user, $db_pass) or die("Unable to connect");
-
+<?php 			
+$db_host = "mysql15.000webhost.com";
+$db_database = "a8591040_jarmain";
+$db_user = "a8591040_admin";
+$db_pass = "jarofjoy123";
+$con=mysqli_connect($db_host, $db_user, $db_pass) or die("Unable to connect");
 ?>
 
 
 
-?> 
